d847a7530dd9f433d73eabe481cc4976dfe5587c
shweta-thakur26 <shwetathakur260199@gmail.com>
2022-10-10 03:15:18 +0530

    Initial Commit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,16 +1,16 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
+    'X-RapidAPI-Key': 'f21fbf6241msh0a72a6905054490p117f0cjsn34adae2ec2ac',
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   },
 };
 
 export const youtubeOptions = {
   method: 'GET',
   headers: {
+    'X-RapidAPI-Key': 'f21fbf6241msh0a72a6905054490p117f0cjsn34adae2ec2ac',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
-    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
   },
 };
 
